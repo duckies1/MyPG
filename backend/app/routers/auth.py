0009c8856d9e0cddb47dc7d9ae1e39a2175b5d4a
duckies1 <adianand1729@gmail.com>
2026-01-25 12:27:02 +0530

    Added login endpoint.

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.schemas.user import UserCreate
+from app.schemas.user import UserCreate, UserLogin
 from app.models.user import User
 from app.core.database import get_db
-from app.core.security import hash_password, create_access_token
+
+from app.core.security import (
+    hash_password,
+    verify_password,
+    create_access_token
+)
 
 ### Authentication Router :- All the auth related routes/endpoints will be defined here
 
@@ -30,3 +35,32 @@ def signup(user: UserCreate, db: Session = Depends(get_db)):
     token = create_access_token({"sub": str(new_user.id)})
 
     return {"access_token": token}
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    # 1. Find user by email
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    # 2. Verify password
+    if not verify_password(user.password, db_user.password_hash):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    # 3. Create JWT token
+    token = create_access_token(
+        {"sub": str(db_user.id)}
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
+
