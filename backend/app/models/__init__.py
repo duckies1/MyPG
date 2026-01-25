ca2d21704207d9125759c54f23697a384af577e3
duckies1 <adianand1729@gmail.com>
2026-01-25 19:04:04 +0530

    small requirements change

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,3 +3,4 @@ from app.models.pg import PG
 from app.models.room import Room
 from app.models.bed import Bed
 from app.models.tenant import TenantProfile  # if exists
+from app.models.rent import Rent
